bc19492509f989ac18918f828d3fee13b8f650d8
PrinzEugen39 <habibsan01@gmail.com>
2024-04-20 13:48:57 +0700

    feat: created the basic layout

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import Question from "@/components/forms/Question";
+import React from "react";
 
 export default function AskQuestion() {
   return (
-    <div>AskQuestion</div>
-  )
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 }
